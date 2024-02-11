5c84276c52ea99a22520be636b316f69eacb1d73
FarahTorkhani <79060004+Farah-Torkhani@users.noreply.github.com>
2024-02-11 17:55:48 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:airecruit/screens/login_scren.dart';
 import 'package:airecruit/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
